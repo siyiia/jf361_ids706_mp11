8afcffa8ebe72ef44b3d25c69e7a42c005cbb04e
siyiia <siyiia>
2024-11-16 15:53:51 +0000

    update

--- lib/query.py
+++ lib/query.py
@@ -6,9 +6,9 @@ from lib.process_data import start_spark, generate_report
 def query(spark, table_name, sql_query, generate_flag=False):
     """Query a Databricks table and generate a markdown report."""
     query_result = spark.sql(sql_query)
-
-    query_result_md = query_result.limit(10).toPandas().to_markdown()
+    
     if generate_flag:
+        query_result_md = query_result.limit(10).toPandas().to_markdown()
         generate_report(content=query_result_md,
                         title=f"Query Result for Table: {table_name}",
                         write_to_file=True,
